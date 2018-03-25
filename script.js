059ac92f41d9da9a8324c6a396e2d0ea35f81ebe
dsdunn <dsdunnmusic@gmail.com>
2018-03-25 10:40:43 -0600

    Update jquery

--- script.js
+++ script.js
@@ -3,13 +3,28 @@ var button = $('.button');
 button.click(getInfo);
 
 function getInfo() {
-  debugger
   event.preventDefault();
+  // var siteTitle = $('#site-title').val();
+ 
   var siteTitle = $('#site-title').val();
   var siteUrl = $('#website-url').val();
-  var newCard = document.createElement('article');
-  var string = document.createTextNode(siteTitle);
-  newCard.appendChild(string);
+  
+  var newCard = 
+  "<article class='card'>"+
+  "<h2 class='card-title'>"
+  +siteTitle+
+  "</h2> <hr> <a href='" + siteUrl + "'class='card-url'>"
+  +siteUrl+
+  "</a> <hr>"+
+  " <button class='read-button'>read</button>"+
+  " <button class='delete'>delete</button>"+
+  "</article";
+
+  $('section').prepend(newCard);
   console.log(newCard);
-  $('section').append(newCard);
+
+  $('.read-button').on('click',function markRead(){
+    $(this).parent().toggleClass("read");
+  })
+
 }
