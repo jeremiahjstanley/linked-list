a6a6016217cc3f42702fda1467c73c8889a68378
dsdunn <dsdunnmusic@gmail.com>
2018-03-26 14:03:35 -0600

    Add button disable and responseive behavior

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ var button = $('.button');
 
 button.click(getInfo);
 
+
+
 function getInfo() {
   event.preventDefault();
  
-  var siteTitle = $('#site-title').val();
-  var siteUrl = $('#website-url').val();
-  console.log(siteTitle);
+var siteTitle = $('#site-title').val();
+var siteUrl = $('#website-url').val();
 
   if(siteTitle === "" || siteUrl === "") {
     displayError();
     return;
   }
-  
+  $(".error").attr('hidden','true');
+
   var newCard = 
   "<article class='card'>"+
   "<h2 class='card-title'>"
@@ -25,12 +27,23 @@ function getInfo() {
   " <button class='delete'>delete</button>"+
   "</article";
 
+  document.querySelector("form").reset();
+
   $('section').prepend(newCard);
 
-    $('section :nth-child(1)').on('click','.read-button', markRead)
-    $('section :nth-child(1)').on('click','.delete', deleteCard)
+  $('section :nth-child(1)').on('click','.read-button', markRead)
+  $('section :nth-child(1)').on('click','.delete', deleteCard)
+
+  button.attr('disabled','true');
 
 }
+
+ $('form').on('input', function() {
+    if($('#site-title').val() && $('#website-url').val()) {
+      button.removeAttr('disabled');
+    }else button.attr('disabled','true');
+  })
+
 function markRead() {
      $(this).parent().toggleClass("read");
 }
@@ -40,5 +53,5 @@ function deleteCard() {
 }
 
 function displayError() {
-  $('.error').toggle('hidden');
+  $('.error').removeAttr('hidden');
 }
