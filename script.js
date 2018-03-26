f955348ef20d15212db23cac65eaf8b92b745ed6
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-03-26 14:10:58 -0600

    Add styling and card counter

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var button = $('.button');
+var total = 0;
+var read = 0;
+var unread = total - read;
 
 button.click(getInfo);
 
 function getInfo() {
-  event.preventDefault();
-  // var siteTitle = $('#site-title').val();
- 
   var siteTitle = $('#site-title').val();
   var siteUrl = $('#website-url').val();
-  
+
+  errorMessage(siteTitle, siteUrl);
+  enableButton(siteTitle, siteUrl);
+
   var newCard = 
   "<article class='card'>"+
   "<h2 class='card-title'>"
@@ -20,11 +23,40 @@ function getInfo() {
   " <button class='delete'>delete</button>"+
   "</article";
 
+  event.preventDefault();
+
   $('section').prepend(newCard);
-  console.log(newCard);
+  $('section :nth-child(1)').on('click','.read-button', markRead);
+  $('section :nth-child(1)').on('click','.delete', deleteCard);
 
-  $('.read-button').on('click',function markRead(){
-    $(this).parent().toggleClass("read");
-  })
+  total +=1
+  console.log(total)
+};
 
-}
+
+function errorMessage(siteTitle, siteUrl) {
+  if(siteTitle === "" || siteUrl === "") {
+    displayError();
+    return;
+  };
+};
+
+function enableButton(siteTitle, siteUrl) {
+  if (siteTitle === "" || siteUrl === "") {
+    button.removeAttribute('disabled', true);
+  };
+};
+
+function markRead() {
+  $(this).parent().toggleClass("read");
+  read +=1
+};
+
+function deleteCard() {
+  $(this).parent().remove();
+  total -=1
+};
+
+function displayError() {
+  $('.error').toggle('hidden');
+};
