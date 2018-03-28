09014c34fccc70910b4527c5be954f386c8ea561
dsdunn <dsdunnmusic@gmail.com>
2018-03-27 18:05:08 -0600

    Add local jQuery libraryand delete read button

--- script.js
+++ script.js
@@ -6,43 +6,41 @@ button.click(getInfo);
 
 
 function getInfo() {
-  event.preventDefault();
- 
-var siteTitle = $('#site-title').val();
-var siteUrl = $('#website-url').val();
+  //event.preventDefault();
+   
+  var siteTitle = $('#site-title').val();
+  var siteUrl = $('#website-url').val();
 
-  if(siteTitle === "" || siteUrl === "") {
-    displayError();
-    return;
-  }
-  $(".error").attr('hidden','true');
+    if(siteTitle === "" || siteUrl === "") {
+      displayError();
+      return;
+    }
+    $(".error").attr('hidden','true');
 
-  var newCard = 
-  "<article class='card'>"+
-  "<h2 class='card-title'>"
-  +siteTitle+
-  "</h2> <hr> <a href='" + siteUrl + "'class='card-url' target='_blank'>"
-  +siteUrl+
-  "</a> <hr>"+
-  " <button class='read-button'>read</button>"+
-  " <button class='delete'>delete</button>"+
-  "</article";
+    var newCard = 
+    "<article class='card'>"+
+    "<h2 class='card-title'>"
+    +siteTitle+
+    "</h2> <hr> <a href='" + siteUrl + "'class='card-url' target='_blank'>"
+    +siteUrl+
+    "</a> <hr>"+
+    " <button class='read-button'>read</button>"+
+    " <button class='delete'>delete</button>"+
+    "</article";
 
-  total +=1
-  tallyRead();
-  
-  
-  
-  document.querySelector("form").reset();
+    document.querySelector("form").reset();
 
-  $('section').prepend(newCard);
+    $('section').prepend(newCard);
 
-  $('section :nth-child(1)').on('click','.read-button', markRead)
-  $('section :nth-child(1)').on('click','.delete', deleteCard)
+    total++;
+    tallyRead();
 
-  button.attr('disabled','true');
+    button.attr('disabled','true');
 
 }
+
+$('section').on('click','.read-button', markRead)
+$('section').on('click','.delete', deleteCard)
 
  $('form').on('input', function() {
     if($('#site-title').val() && $('#website-url').val()) {
@@ -72,8 +70,24 @@ function displayError() {
 }
 
 function tallyRead(){
-  
+  if (read) {
+    $('.delete-read').removeAttr('disabled');
+  }
   $('.total').text("Total: " + total);
   $('.read-count').text("Read: " + read);
   $('.unread').text("Unread: " + (total - read));
 }
+
+
+ $('.delete-read').on('click',function(e){
+  e.preventDefault();
+    $('.read').remove();
+    total = total - read;
+    read = 0;
+    tallyRead();
+  })
+
+
+
+
+
